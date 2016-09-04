7a6de252813ca1e21a6c99e1a2ff1ebd2503e8d5
sgtDentons <sgt.dentons@gmail.com>
2016-09-04 20:12:51 +0200

    Teraz z 200% więcej testów w testach

--- src/test/java/HttpRiotClientTest.java
+++ src/test/java/HttpRiotClientTest.java
@@ -1,6 +1,53 @@
 /**
  * Created by QDL on 2016-09-04.
  */
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.model.Player;
+import pl.service.riot.HttpRiotClient;
+import pl.service.wargaming.HttpWargamingClient;
+
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class HttpRiotClientTest
 {
+    @Spy
+    private Player mockPlayer;
+
+
+    @InjectMocks
+    private HttpRiotClient testObject = new HttpRiotClient();
+
+
+
+    @Test
+    public void testFindById()
+    {
+        Player testPlayer = testObject.findPlayerById("19940663");
+
+        //assertEquals("SIPIAIM",testPlayer.getName()); //jak się okazuje, klient nie pobiera nazwy użytkownika
+        assertEquals((Integer) 15,testPlayer.getWins());
+        assertEquals((Integer) 6,testPlayer.getLosses());
+        assertEquals((Integer) 147,testPlayer.getKills());
+    }
+
+    @Test
+    public void testFindByName()
+    {
+        Player testPlayer = testObject.findPlayerByName("SIPIAIM");
+
+        assertEquals("SIPIAIM",testPlayer.getName());
+
+        assertEquals((Integer) 15,testPlayer.getWins());
+        assertEquals((Integer) 6,testPlayer.getLosses());
+        assertEquals((Integer) 147,testPlayer.getKills());
+    }
+
 }
--- src/test/java/MockAutowireTest.java
+++ src/test/java/MockAutowireTest.java
@@ -1,6 +1,45 @@
 /**
  * Created by QDL on 2016-09-04.
  */
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.model.Player;
+import pl.service.wargaming.HttpWargamingClient;
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MockAutowireTest
 {
+    @Mock
+    private Player mockPlayer;
+
+
+    @InjectMocks
+    private HttpWargamingClient testObject = new HttpWargamingClient();
+
+
+//    @Before
+//    public void initMocs(){
+//        MockitoAnnotations.initMocks(this);
+//    }
+
+    @Test
+    public void testAutowire()
+    {
+
+//        HttpWargamingClient testObject = new HttpWargamingClient("kaimada");
+        //HttpWargamingClient testObject = new HttpWargamingClient();
+        assertNotNull(testObject);
+        assertNotNull(testObject.player);
+    }
 }
